00c359c35a7e1fad56aa501dda3ec8f9250144e6
Swapratim <Swapratim@users.noreply.github.com>
2017-06-22 11:14:42 +0200

    Add files via upload

--- context.py
+++ context.py
@@ -392,7 +392,8 @@ def searchhook():
         pagemap = data_item['pagemap'],
 
     #for key in pagemap:
-        print ('pagemap contains cse_thumbnail? ' + pagemap.has_key('cse_thumbnail'))
+    print (pagemap)
+    print ('pagemap contains cse_thumbnail? ' + pagemap.has_key('cse_thumbnail'))
         if key['cse_thumbnail'] in pagemap:
             cse_thumbnail = key['cse_thumbnail'],
             for image_data in cse_thumbnail:
