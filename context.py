2758d016fcd6012125b717bcd8e083cabeb4b96e
Swapratim <Swapratim@users.noreply.github.com>
2017-06-22 09:14:12 +0200

    Add files via upload

--- context.py
+++ context.py
@@ -394,7 +394,7 @@ def searchhook():
     for key in pagemap:
         if key['cse_thumbnail']:
             cse_thumbnail = key['cse_thumbnail'],
-                for image_data in cse_thumbnail:
+            for image_data in cse_thumbnail:
                     raw_str = image_data['src'],
         else:
             raw_str = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwdc3ra_4N2X5G06Rr5-L0QY8Gi6SuhUb3DiSN_M-C_nalZnVA",
