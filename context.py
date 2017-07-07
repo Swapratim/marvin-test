b4853ced899f7972070f118dcce4b59ab3c9f9a9
Swapratim <Swapratim@users.noreply.github.com>
2017-07-07 16:02:19 +0200

    Add files via upload

--- context.py
+++ context.py
@@ -106,7 +106,30 @@ def welcome():
                  {
                  "text": speech
                   },
-                 ]
+                 {
+                  "text": "Click your choice:",
+                  "quick_replies": [
+                 {
+                  "content_type": "text",
+                  "title": "News",
+                  "payload": "news",
+                  "image_url": "http://www.freeiconspng.com/uploads/newspaper-icon-20.jpg"
+                 },
+                 {
+                  "content_type": "text",
+                  "title": "Weather",
+                  "payload": "weather",
+                  "image_url": "https://www.mikeafford.com/store/store-images/ww01_example_light_rain_showers.png"
+                   },
+                  {
+                  "content_type": "text",
+                  "title": "Wikipedia",
+                  "payload": "wikipedia",
+                  "image_url": "https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1122px-Wikipedia-logo-v2.svg.png"
+                   }
+                  ]
+                 }
+                ]
                }
             };
     print (res)
