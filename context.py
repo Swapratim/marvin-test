05004df66aba13d14eef23e06515bd358e14121f
Swapratim <Swapratim@users.noreply.github.com>
2017-10-02 16:29:46 +0200

    Add files via upload

--- context.py
+++ context.py
@@ -37,8 +37,8 @@ weather_update_key = "747d84ccfe063ba9"
 def webhook():
     reqContext = request.get_json(silent=True, force=True)
     #print(json.dumps(reqContext, indent=4))
-    #print(reqContext.get("result").get("action"))
-    print ("webhook is been hit ONCE ONLY")
+    print(reqContext.get("result").get("action"))
+    #print ("webhook is been hit ONCE ONLY")
     if reqContext.get("result").get("action") == "input.welcome":
        return welcome()
     elif reqContext.get("result").get("action") == "weather":
